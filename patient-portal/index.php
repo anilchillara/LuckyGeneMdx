6580449a95ce4f945700fd7a066bd0b524446eda
Anil Chillara <Anil.Chillara@outlook.com>
2026-02-16 16:57:38 -0600

    Fixing style

--- patient-portal/index.php
+++ patient-portal/index.php
@@ -85,7 +85,7 @@ $firstName = explode(' ', $userName)[0];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - LuckyGeneMDx Patient Portal</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="/css/main.css">
     <style>
         .portal-wrapper { display: flex; min-height: 100vh; }
         .portal-sidebar {
